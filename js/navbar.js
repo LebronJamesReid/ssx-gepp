eaa8b684e9d3a84029163d3c24e6dc5681b9c1d5
Kenneth Gabriel G. Tecson <kgtecson@citem.dti>
2025-03-10 17:41:10 +0800

    6 pages optimize | ongoing for animation

--- js/navbar.js
+++ js/navbar.js
@@ -15,12 +15,16 @@ function setupNavbarScroll() {
 
 function setupActiveNavLinks() {
   const navLinks = document.querySelectorAll(".nav-link");
+  const aboutGreenEconomyLink = document.querySelector("#about-green-economy-link");
+  const learningCenterLink = document.querySelector("#learning-center-link");
+  const collaborationsLink = document.querySelector("#collaborations-link")
   if (!navLinks.length) return;
 
   const currentPath = window.location.pathname; // Get only the path (without domain)
 
   navLinks.forEach((link) => {
-    const linkPath = new URL(link.href).pathname; // Extract path from href
+    const linkHref = link.getAttribute('href'); // Get the href attribute of the link
+    const linkPath = new URL(linkHref, window.location.origin).pathname; // Resolve the relative URL
 
     // Check if link matches the current path
     if (
@@ -30,12 +34,41 @@ function setupActiveNavLinks() {
       link.classList.add("active");
     }
 
-    link.addEventListener("click", function () {
-      navLinks.forEach((el) => el.classList.remove("active"));
-      this.classList.add("active");
-    });
+    // link.addEventListener("click", function () {
+    //   navLinks.forEach((el) => el.classList.remove("active"));
+    //   this.classList.add("active");
+    // });
   });
+
+  // Check if the current page  green-economy.html or about-gepp.html
+  if (currentPath === "/green-economy.html" || currentPath === "/about-gepp.html") {
+    // Add active class to the "About Green Economy" link
+    if (aboutGreenEconomyLink) {
+      const dropdownToggle = aboutGreenEconomyLink.querySelector('.nav-link');
+      dropdownToggle.classList.add("active");
+    }
+  }
+
+    // Check if the current page  knowledege hub
+    if (currentPath === "/knowledge-hub.html") {
+      // Add active class to the "About Green Economy" link
+      if (learningCenterLink) {
+        const dropdownToggle = learningCenterLink.querySelector('.nav-link');
+        dropdownToggle.classList.add("active");
+      }
+    }
+
+            // Check if the current page  knowledege hub
+            if (currentPath === "/collaborations.html") {
+              // Add active class to the "About Green Economy" link
+              if (collaborationsLink) {
+                const dropdownToggle = collaborationsLink.querySelector('.nav-link');
+                dropdownToggle.classList.add("active");
+              }
+            }
 }
+
+
 
 function chevronDropDown() {
   const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
